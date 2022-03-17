db851d7d71e7f11f47cb131b40747f49a45af333
firasel <mdrasel601390@gmail.com>
2022-03-18 01:19:15 +0600

    hero section responsive done

--- components/Home/Hero/Hero.js
+++ components/Home/Hero/Hero.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
+import Ellipse from "../../../assets/images/Hero/Ellipse1.svg";
+import Polygon from "../../../assets/images/Hero/Polygon1.svg";
 import ProfileImg from "../../../assets/images/Hero/profile01.jpg";
+import Rectangle from "../../../assets/images/Hero/Rectangle.svg";
 import style from "./Hero.module.scss";
 
 const Hero = () => {
   return (
-    <div className={`container overflow-hidden ${style.heroSection}`}>
-      <Row className=" heroContainer">
-        <Col md={5} className="d-flex align-items-center">
+    <div className={`container sectionStyle ${style.heroSection}`}>
+      <Row className="heroContainer m-0 p-0">
+        <Col md={6} lg={5} className="d-flex align-items-center">
+          {/* Hero details start */}
           <div className="heroDetails">
             <h5>Senior UX/UI Designer</h5>
             <h2>
@@ -16,7 +20,7 @@ const Hero = () => {
               John Doe
             </h2>
             <p>
-              I am a freelancer based in the United State and I've been
+              I am a freelancer based in the United State and I&apos;ve been
               constructing noteworthy UX/UI designs and websites for years,
               which comply with the brand new layout trends. I help convert an
               imaginative and prescient idea into significant and beneficial
@@ -24,10 +28,29 @@ const Hero = () => {
             </p>
             <Button>Hire Me</Button>
           </div>
+          {/* Hero details end */}
         </Col>
-        <Col md={4} className="d-flex align-items-center offset-2">
+        <Col md={6} lg={4} className="d-flex align-items-center offset-lg-2">
           <div className="heroImage">
-            <Image src={ProfileImg} alt="hero image" />
+            {/* Hero profile image */}
+            <Image layout="responsive" src={ProfileImg} alt="hero image" />
+            {/*  */}
+            {/* Image background style start*/}
+            <div>
+              <div className="circle1">
+                <Image src={Ellipse} alt="ellipse" />
+              </div>
+              <div className="circle2">
+                <Image src={Ellipse} alt="ellipse" />
+              </div>
+              <div className="rectangle">
+                <Image src={Rectangle} alt="ellipse" />
+              </div>
+              <div className="polygon">
+                <Image src={Polygon} alt="ellipse" />
+              </div>
+            </div>
+            {/* Image background style end*/}
           </div>
         </Col>
       </Row>
