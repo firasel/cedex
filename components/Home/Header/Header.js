ed295007a147108fc8bba1bb193845be94535146
firasel <mdrasel601390@gmail.com>
2022-03-17 20:16:42 +0600

    navbar 50% done

--- components/Home/Header/Header.js
+++ components/Home/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../shared/Navbar/Navbar";
+import Hero from "../Hero/Hero";
 
 const Header = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
--- components/shared/Navbar/Navbar.js
+++ components/shared/Navbar/Navbar.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import LogoImg from "../../../assets/images/Logo.png";
+import style from "./Navbar.module.scss";
 
-const Navbar = () => {
-    return (
-        <div>
-            This is navbar
-        </div>
-    );
+const NavBar = () => {
+  return (
+    <>
+      <Navbar
+        className={style.navbarStyle}
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand className="logo py-4" href="#">
+            <Image src={LogoImg} alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto menuItems">
+              <Nav.Link as={Link} href="/">Home</Nav.Link>
+              <Nav.Link as={Link} href="/portfolio">Portfolio</Nav.Link>
+              <Nav.Link as={Link} href="/blogs">Blogs</Nav.Link>
+              <Nav.Link as={Link} href="/contact">Contact</Nav.Link>
+              <Nav.Link as={Link} href="/about">About us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
-export default Navbar;
+export default NavBar;
