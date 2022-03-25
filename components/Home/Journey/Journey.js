2921690874fe3daea73c9e14fb1c2f0ee763676e
firasel <mdrasel601390@gmail.com>
2022-03-25 23:37:57 +0600

    journey section animation added

--- components/Home/Journey/Journey.js
+++ components/Home/Journey/Journey.js
@@ -1,9 +1,24 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import journeyData from "../../../data/journeyData";
 import style from "./Journey.module.scss";
 
 const Journey = () => {
+  const variants = {
+    hover: {
+      rotate: [0, 5, 0],
+      scale: [1, 1.07, 1],
+      transition: {
+        duration: 0.8,
+      },
+    },
+    rest: {
+      scale: 1,
+      rotate: 0,
+    },
+  };
+
   return (
     <div className={`${style.journeyStyle} sectionStyle`}>
       <Container>
@@ -18,13 +33,32 @@ const Journey = () => {
         {/* Cards start */}
         <Row className="justify-content-md-between">
           {journeyData?.jobs?.map((data, index) => (
-            <Col key={index} md={6} lg={5} className="journeyCard">
-              <div className="logo">{data?.icon}</div>
-              <div className="content">
-                <h4>{data?.position}</h4>
-                <span>{data?.companyName}</span>
-                <p>{data?.time}</p>
-              </div>
+            <Col key={index} md={6} lg={5}>
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 0.3, delay: index * 0.2 },
+                }}
+                viewport={{ once: true }}
+              >
+                <motion.div
+                  initial="rest"
+                  whileHover="hover"
+                  animate="rest"
+                  className="journeyCard"
+                >
+                  <motion.div variants={variants} className="logo">
+                    {data?.icon}
+                  </motion.div>
+                  <div className="content">
+                    <h4>{data?.position}</h4>
+                    <span>{data?.companyName}</span>
+                    <p>{data?.time}</p>
+                  </div>
+                </motion.div>
+              </motion.div>
             </Col>
           ))}
         </Row>
